9dabf3e8161f4845796e009aefaaa573ab4bfd80
iAmNathanJ <jacobs@heysparkbox.com>
2016-01-27 22:53:11 -0500

    update publish.js: add read function

--- publish.js
+++ publish.js
@@ -1,3 +1,12 @@
 'use strict';
 
 let fs = require('fs');
+
+function read(src, dest) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(src, 'utf8', (err, contents) => {
+      if(err) reject(new Error(err));
+      resolve(contents);
+    });
+  });
+}
