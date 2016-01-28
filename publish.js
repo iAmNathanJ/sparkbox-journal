b2b5801325e6a061530301cee89540bc7ede832b
iAmNathanJ <jacobs@heysparkbox.com>
2016-01-28 10:54:26 -0500

    update publish.js: working base script

--- publish.js
+++ publish.js
@@ -1,12 +1,44 @@
 'use strict';
 
-let fs = require('fs');
+let fs    = require('fs')
+  , path  = require('path')
+  , read  = fs.createReadStream
+  , write = fs.createWriteStream;
 
-function read(src, dest) {
+let file      = process.argv[2]
+  , filename  = path.basename(file)
+  , dest      = `./entries/${filename}`;
+
+
+function validateSrc(fSrc) {
   return new Promise((resolve, reject) => {
-    fs.readFile(src, 'utf8', (err, contents) => {
-      if(err) reject(new Error(err));
-      resolve(contents);
+    fs.stat(fSrc, (err, stats) => {
+      if(err) reject(new Error(`Source file doesn't exist: ${fSrc}`));
+      resolve(fSrc);
     });
   });
 }
+
+function validateDest(fSrc, fDest) {
+  return new Promise((resolve, reject) => {
+    fs.stat(fDest, (err, stats) => {
+      if(err) resolve(`Copying ${fSrc} => ${fDest}`);
+      reject(new Error(`File already exists: ${fDest}`));
+    });
+  });
+}
+
+validateSrc(file)
+.then((src) => {
+  return validateDest(src, dest);
+})
+.then((msg) => {
+  console.log(msg);
+  read(file)
+  .pipe(write(dest));
+})
+.catch((err) => {
+  console.log(err);
+});
+
+// clean up draft
