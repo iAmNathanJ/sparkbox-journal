fc05951201debd54449e51ec27d6fb07a5973fde
iAmNathanJ <jacobs@heysparkbox.com>
2016-01-30 22:47:21 -0500

    update publish.js: add README to commit

--- publish.js
+++ publish.js
@@ -135,7 +135,7 @@ Push to Github
 ==========================================
 */
 function yolo() {
-  let add = spawn('git', ['add', dest + ' README.md']);
+  let add = spawn('git', ['add', dest, 'README.md']);
   let commit = spawn('git', ['commit', '-m', `add ${filename} and update README`]);
   let push = spawn('git', ['push']);
   [add, commit, push].forEach((cmd) => {
